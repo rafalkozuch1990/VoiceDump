4bd0b63d8cc1dfcc4591d974a229cf665999532e
rafalkozuch1990 <tmp48gkzvf@privaterelay.appleid.com>
2026-09-22 10:06:16 +0200

    Create types.ts

---
+++ lib/types.ts
@@ -0,0 +1,13 @@
+export type Task = {
+  id: string;
+  text: string;
+  done: boolean;
+};
+
+export type Note = {
+  id: string;
+  title: string;
+  summary: string;
+  tasks: Task[];
+  createdAt: string;
+};
